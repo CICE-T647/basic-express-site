5a3a602a341e6b2efbff415a9eca3fca351030a7
meji <soy@meji.es>
2019-12-02 22:01:21 +0100

    comienzo a usar query 2.0

--- app.js
+++ app.js
@@ -2,18 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+
 app.get("/query", (req, res) => {
     const query = req.query;
     console.log("Query", query); 
-    res.send());//Send
-}
-app.get("/query", (req, res) => {
-    const query = req.query;
-    console.log("Query", query); 
-    res.send());//Send
-}
+    res.send();//Send
+});
 app.get("/home", (req, res) => res.sendFile(__dirname+"/views/home.html"));
 app.get("/about", (req, res) => res.sendFile(__dirname+"/views/about.html"));
 app.get("/gallery", (req, res) => res.sendFile(__dirname+"/views/gallery.html"));
 app.use(express.static(__dirname+"/public")); //Para que use de forma pública esta carpeta. 
 app.listen(PORT, ()=> console.log(`servidor corriendo en puerto ${PORT}`));
+ 
