02c039c2e7b0c65b056e33a9374dd02c1b593ba7
olatzaranzabe <olatzaranzabe@gmail.com>
2019-12-02 21:47:25 +0100

    start express server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/home", (req, res) => res.sendFile(__dirname + "/views/home.html"));
+
+app.get("/about", (res, req) => res.sendFile(__dirname + "views/about.html"));
+
+app.get("/thankspage", (res, req) =>
+    res.sendFile(__dirname + "views/thankspage.html")
+);
+
+app.use(express.static(__dirname + "/public"));
+app.get("/html", (req, res) => {
+    console.log(__dirname);
+    res.sendFile(__dirname + "/views/home.html");
+});
+
+app.listen(PORT, () => console.log("servidor corriendo en puerto 3000"));
